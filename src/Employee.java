8d059a4685e089552ba09acbcfdc67acb6b7dc8e
darya <darya.kolycheva98@yandex.ru>
2024-04-05 13:51:58 +0300

    Исправила работу

--- src/Employee.java
+++ src/Employee.java
@@ -2,15 +2,13 @@ import java.util.Objects;
 
 public class Employee {
     private String fullNameEmployee;
-    private int departmentNumberEmployee;
     private int salaryEmployee;
-    static int enumerator=1;
-    private int id=1;
+    private static int enumerator = 1;
+    private int id;
     private int dept;
 
-    public Employee(String fullNameEmployee, int departmentNumberEmployee, int salaryEmployee, int dept) {
+    public Employee(String fullNameEmployee, int salaryEmployee, int dept) {
         this.fullNameEmployee = fullNameEmployee;
-        this.departmentNumberEmployee = departmentNumberEmployee;
         this.salaryEmployee = salaryEmployee;
         this.id = enumerator++;
         this.dept = dept;
@@ -20,19 +18,12 @@ public class Employee {
         return this.fullNameEmployee;
     }
 
-    public int getDepartmentNumberEmployee() {
-        return this.departmentNumberEmployee;
-    }
     public int getDept() {
         return this.dept;
     }
 
     public int getSalaryEmployee() {
         return this.salaryEmployee;
-    }
-
-    public void setDepartmentNumberEmployee(byte departmentNumberEmployee) {
-        this.departmentNumberEmployee = departmentNumberEmployee;
     }
 
     public void setSalaryEmployee(int salaryEmployee) {
@@ -47,16 +38,16 @@ public class Employee {
             return false;
         }
         Employee employee = (Employee) o;
-        return departmentNumberEmployee == employee.departmentNumberEmployee && salaryEmployee == employee.salaryEmployee &&
-                id == employee.id && Objects.equals(fullNameEmployee, employee.fullNameEmployee);
-
-    }
+        return salaryEmployee == employee.salaryEmployee &&
+                id == employee.id && dept == employee.dept &&
+                Objects.equals(fullNameEmployee, employee.fullNameEmployee);
+        }
 
     @Override
     public int hashCode() {
-        return Objects.hash(fullNameEmployee,departmentNumberEmployee, salaryEmployee, id);
+        return Objects.hash(fullNameEmployee, salaryEmployee, id, dept);
     }
     public String toString(){
-        return  "Работник № " + departmentNumberEmployee + ". Сотрудник (ФИО):'" + fullNameEmployee  + ", зарплата: " + salaryEmployee + ", отдел " + dept + ".";
+        return  "Работник № " + id + ". Сотрудник (ФИО):'" + fullNameEmployee  + ", зарплата: " + salaryEmployee + ", отдел " + dept + ".";
     }
-}
+    }
--- src/Main.java
+++ src/Main.java
@@ -3,54 +3,80 @@ public class Main {
     private static Employee[] employees = new Employee[10];
 
     static void base() {
-        employees[0] = new Employee("Безрукавый Вадим Петрович ", Employee.enumerator, 50000, 1);
-        employees[1] = new Employee("Коток Алексей Юрьевич ", Employee.enumerator, 90000, 2);
-        employees[2] = new Employee("Реунов Константин Юрьевич ", Employee.enumerator , 70000, 3);
-        employees[3] = new Employee("Постов Андрей Юрьевич ", Employee.enumerator, 60000, 4);
-        employees[4] = new Employee("Филиппов Алексей Павлович ", Employee.enumerator, 50000, 5);
-        employees[5] = new Employee("Чугунов Сергей Вячеславович ", Employee.enumerator, 70000, 1);
-        employees[6] = new Employee("Сырятов Денис Сергеевич ", Employee.enumerator, 50000, 2);
-        employees[7] = new Employee("Ищенко Александр Сергеевич ", Employee.enumerator, 40000, 3);
-        employees[8] = new Employee("Соловьев Дмитрий Геннадьевич ", Employee.enumerator, 50000, 4);
-        employees[9] = new Employee("Корников Алексей Александрович ", Employee.enumerator, 100000, 5);
+        employees[0] = new Employee("Безрукавый Вадим Петрович ", 50000, 1);
+        employees[1] = new Employee("Коток Алексей Юрьевич ", 90000, 2);
+        employees[2] = new Employee("Реунов Константин Юрьевич ", 70000, 3);
+        employees[3] = new Employee("Постов Андрей Юрьевич ", 60000, 4);
+        employees[4] = new Employee("Филиппов Алексей Павлович ", 50000, 5);
+        employees[5] = new Employee("Чугунов Сергей Вячеславович ", 70000, 1);
+        employees[6] = new Employee("Сырятов Денис Сергеевич ", 50000, 2);
+        employees[7] = new Employee("Ищенко Александр Сергеевич ", 40000, 3);
+        employees[8] = new Employee("Соловьев Дмитрий Геннадьевич ", 50000, 4);
+        employees[9] = new Employee("Корников Алексей Александрович ", 100000, 5);
     }
 
     public static void main(String[] args) {
         base();
         printEmployee();
-        printMin_MaxSalaryEmployee();
+        getSalaryEmployees();
         System.out.println("Сумма затрат на ЗП в месяц: " + getSalaryEmployees());
-        System.out.println("Среднее значение зарплат: " + getSalaryEmployees() / employees.length);
-        printEmployee();
+        printEqualSalary();
+        System.out.println("Среднее значение зарплат: " + printEqualSalary());
+        printMinSalaryEmployee();
+        System.out.println("Минимальная зарплата у сотрудника: " + printMinSalaryEmployee());
+        printMaxSalaryEmployee();
+        System.out.println("Максимальная зарплата у сотрудника: " + printMaxSalaryEmployee());
+        printEmployeeFullName();
     }
+
     private static void printEmployee() {
-        for (int i = 0; i < employees.length; i++)
+        for (int i = 0; i < employees.length; i++) {
             System.out.println(employees[i]);
+        }
     }
 
     private static int getSalaryEmployees() {
         int sum = 0;
-        for (int i = 0; i < employees.length; i++) {
-            sum +=employees[i].getSalaryEmployee();
+        for(int i = 0; i < employees.length; i++) {
+            sum += employees[i].getSalaryEmployee();
         }
         return sum;
     }
-        private static void printMin_MaxSalaryEmployee () {
-            int minSalaryEmployee = employees[0].getSalaryEmployee();
-            int maxSalaryEmployee = employees[0].getSalaryEmployee();
-            String employeesMinSalary = employees[0].getFullNameEmployee();
-            String employeesMaxSalary = employees[0].getFullNameEmployee();
-            for (int i = 1; i < employees.length; i++) {
-                if (employees[i].getSalaryEmployee() < minSalaryEmployee) {
-                    minSalaryEmployee = employees[i].getSalaryEmployee();
-                    employeesMinSalary = employees[i].getFullNameEmployee();
-                }
-                if (employees[i].getSalaryEmployee() > maxSalaryEmployee) {
-                    maxSalaryEmployee = employees[i].getSalaryEmployee();
-                    employeesMaxSalary = employees[i].getFullNameEmployee();
-                }
+    private static int printEqualSalary() {
+        int sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            sum += employees[i].getSalaryEmployee() / employees.length;
+        }
+        return sum;
+    }
+
+    private static String printMinSalaryEmployee() {
+        int minSalaryEmployee = employees[0].getSalaryEmployee();
+        String employeesMinSalary = employees[0].getFullNameEmployee();
+        for (int i = 1; i < employees.length; i++) {
+            if (employees[i].getSalaryEmployee() < minSalaryEmployee) {
+                minSalaryEmployee = employees[i].getSalaryEmployee();
+                employeesMinSalary = employees[i].getFullNameEmployee();
             }
-            System.out.println("Минимальная зарплата у сотрудника: " + employeesMinSalary + " равна: " + minSalaryEmployee);
-            System.out.println("Максимальная зарплата у сотрудника: " + employeesMaxSalary + " равна: " + maxSalaryEmployee);
+        }
+        return employeesMinSalary;
+    }
+
+    private static String printMaxSalaryEmployee() {
+        int maxSalaryEmployee = employees[0].getSalaryEmployee();
+        String employeesMaxSalary = employees[0].getFullNameEmployee();
+        for (int i = 1; i < employees.length; i++) {
+            if (employees[i].getSalaryEmployee() > maxSalaryEmployee) {
+                maxSalaryEmployee = employees[i].getSalaryEmployee();
+                employeesMaxSalary = employees[i].getFullNameEmployee();
+            }
+        }
+        return employeesMaxSalary;
+    }
+
+    private static void printEmployeeFullName() {
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println("ФИО: " + employees[i].getFullNameEmployee());
         }
     }
+}
